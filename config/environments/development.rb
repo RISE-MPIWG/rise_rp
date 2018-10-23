d446350a894c1b2fa783f666996992c6f692420f
Pascal Belouin <pbelouin@mpiwg-berlin.mpg.de>
2018-10-23 16:33:22 +0200

    eager load for dev

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -7,6 +7,8 @@ RiseRp::Application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
+  config.eager_load = false
+
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
 
